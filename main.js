b91b719c6ad00017bb4ba6deb5315c59dc921ad4
Sylvester Mbah <sylvestermbah90@gmail.com>
2022-10-12 08:51:57 -0700

    Create a function that plays single round of rock-paper-scissor

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+let choices = ['rock', 'paper', 'scissor'];
 
 function getComputerChoice() {
-    let choices = ['rock', 'paper', 'scissor'];
-    let computerChoice = choices[Math.floor(Math.random() * choice.length)];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        return 'You won! Rock beats Scissor';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lost! Paper beats Rock'; 
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You won! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        return 'You lost! Scissor beats Paper';
+    } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        return 'You lost! Rock beats Scissor';
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        return 'You won! Scissor beats Paper';
+    } else {
+        return 'It\'s a draw!';
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
